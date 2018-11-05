662b6f28c4194885335bf999d8caf512292ef4f9
soal <sorrow.about.alice@gmail.com>
2018-11-05 06:27:23 +0300

    Added tslint configuration

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import promisifyMap from './promisifyMap'
 import promisifyMethod from './promisifyMethod'
 import { Map } from 'mapbox-gl'
 
-export default function promisify (map: Map, methodName: string = null): Function | Map {
+export default function promisify(
+  map: Map,
+  methodName: string = null
+): ((...args: any) => Promise<object>) | Map {
   if (methodName) {
     return promisifyMethod(map, methodName)
   } else {
