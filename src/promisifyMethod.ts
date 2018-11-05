9d81ea21381ce15952a03ad8f0aa5e98193b9d95
soal <sorrow.about.alice@gmail.com>
2018-11-05 06:33:34 +0300

    Functions types updated

--- src/promisifyMethod.ts
+++ src/promisifyMethod.ts
@@ -1,5 +1,7 @@
 import methods from './methodsData'
-import { Map } from 'mapbox-gl'
+import { Map, MapboxEvent } from 'mapbox-gl'
+
+type Event = { eventId: string } & MapboxEvent
 
 function generateEventId (methodName: string): string {
   return `${methodName}-${('' + Math.random()).split('.')[1]}`
@@ -9,23 +11,25 @@ function catchEventFabric(
   map: Map,
   eventName: string,
   eventId: string,
-  resolve: Function
+  resolve: (event) => void
 ): (event: any) => void {
-  const catchEvent = event => {
-    if (event.type !== eventName || event.eventId !== eventId) return
+  const catchEvent = (event: Event) => {
+    if (event.type !== eventName || event.eventId !== eventId) {
+      return
+    }
     map.off(eventName, catchEvent)
     return resolve(event)
   }
   return catchEvent
 }
 
-function promisifyMethod (map: Map, methodName: string): (any) => Promise<object> {
+function promisifyMethod(map: Map, methodName: string): (any) => Promise<object> {
   const method = map[methodName]
   const argsCount = method.length
 
   return (...args) => {
     const eventData = { eventId: generateEventId(methodName) }
-    const funcs = methods[methodName].events.map(event => {
+    const funcs = methods[methodName].events.map((event) => {
       return new Promise((resolve, reject) => {
         map.on(event, catchEventFabric(map, event, eventData.eventId, resolve))
       })
